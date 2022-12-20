e6b6e2943eaf1f7c1c526bb1015ae4d3129f347f
Hubert EDOA <edoahubert@gmail.com>
2022-12-20 19:36:01 +0100

    Déplacement d'une image

--- interface.py
+++ interface.py
@@ -7,30 +7,48 @@ class Interface:
         pygame.init()
         self.ecran_long,self.ecran_larg = 1100, 750
         self.fenetre = pygame.display.set_mode((self.ecran_long, self.ecran_larg), pygame.RESIZABLE)
-        self.image_de_fond = pygame.image.load('Images/bg2.jpg')
-        actif = 1
+        self.image_de_fond_menu = pygame.image.load('Images/bg2.jpg')
+        self.actif = 1
 
-        while actif: 
-            self.image_de_fond = pygame.transform.scale(self.image_de_fond,(self.fenetre.get_width(),self.fenetre.get_height()))
-            self.fenetre.blit(self.image_de_fond,(0,0))
+        while self.actif: 
+            self.image_de_fond_menu = pygame.transform.scale(self.image_de_fond_menu,(self.fenetre.get_width(),self.fenetre.get_height()))    
+            self.fenetre.blit(self.image_de_fond_menu,(0,0))
             self.menu()
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    actif = 0
+                    self.actif = 0
                 if event.type == KEYDOWN:
                     if event.key == K_RETURN:
-                        actif = 1
+                        self.actif = 1
             pygame.display.update()
         pygame.quit()
 
     def menu(self):
         #btn_newgame = pygame.image.load()
         #btn_lastgame = pygame.image.load()
-        btn_parametres = pygame.image.load('Images/settings.jpg').convert_alpha()
+        btn_parametres = pygame.image.load("./Images/Cartes/2 Carreau 1.png")
+        btn_parametres = pygame.transform.scale(btn_parametres,(145,197))
         #btn_quitter = pygame.image.load()
         rect = btn_parametres.get_rect()
-        rect.bottomright = (100,100)
-        self.fenetre.blit(btn_parametres, (rect.x, rect.y))
+        rect.center = 145//2, 197//2
+        rect.topleft = (0,0)
+        running = True
+        moving = False
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                    self.actif = 0
+                elif event.type == MOUSEBUTTONDOWN:
+                    if rect.collidepoint(event.pos):
+                        moving = True
+                elif event.type == MOUSEBUTTONUP:
+                    moving = False
+                elif event.type == MOUSEMOTION and moving:
+                    rect.move_ip(event.rel)
+            self.fenetre.blit(self.image_de_fond_menu,(0,0))
+            self.fenetre.blit(btn_parametres,rect)
+            pygame.display.update()
 
     def parametres(self):
         pass
@@ -39,6 +57,10 @@ class Interface:
         pass
 
     def continuer_partie(self):
+        pass
+
+    def menu_de_jeu(self):
+
         pass
 
 
